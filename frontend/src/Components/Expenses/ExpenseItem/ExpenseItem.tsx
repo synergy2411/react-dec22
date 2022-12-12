57364d9800a4e2404d9c6fa48f4d3bea5d5e3312
Sumit Khandelwal <synergy2411@outlook.com>
2022-12-12 15:23:35 +0530

    expense item 2

--- frontend/src/Components/Expenses/ExpenseItem/ExpenseItem.tsx
+++ frontend/src/Components/Expenses/ExpenseItem/ExpenseItem.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 import IExpense from "../../../model/expense.interface";
 
-const ExpenseItem: React.FC<{ expense: IExpense }> = (props) => {
-
+// const ExpenseItem: React.FC<{ expense: IExpense }> = (props) => {
+const ExpenseItem = (props: { expense: IExpense }) => {
 
     return (
         <div className="col-4">
@@ -10,7 +10,7 @@ const ExpenseItem: React.FC<{ expense: IExpense }> = (props) => {
                 <div className="card-header">
                     <h6 className="text-center">{props.expense.title.toUpperCase()}</h6>
                 </div>
-                <div className="card-bod">
+                <div className="card-body">
                     <p>Amount : ${props.expense.amount}</p>
                     <p>Created At : {props.expense.createdAt.toString()} </p>
                 </div>
